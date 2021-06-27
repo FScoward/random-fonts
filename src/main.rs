12000bf4fc7f42c9cd6a6a8f81bc2378673c3a21
FScoward <endlick@gmail.com>
2021-06-27 14:40:36 +0900

    :construction: wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::collections::HashMap;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     let path_ame = "GFONT_API_KEY";
-    println!("{}", env::var(path_ame).expect("GFONT_API_KEY not found."))
+    let api_key = env::var(path_ame).expect("GFONT_API_KEY not found.");
+    println!("{}", api_key);
+
+    let web_fonts_url = "https://www.googleapis.com/webfonts/v1/webfonts?key=";
+
+    println!("{}", format!("{}{}", web_fonts_url, api_key));
+    let resp = reqwest::get(format!("{}{}", web_fonts_url, api_key))
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+
 }
