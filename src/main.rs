15c95dc38c62da8b8c0f784fb198185f71b50bd2
FScoward <endlick@gmail.com>
2021-06-27 17:14:50 +0900

    :construction: wip

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use std::collections::HashMap;
 use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-
     let path_ame = "GFONT_API_KEY";
     let api_key = env::var(path_ame).expect("GFONT_API_KEY not found.");
     println!("{}", api_key);
 
     let web_fonts_url = "https://www.googleapis.com/webfonts/v1/webfonts?key=";
 
-    println!("{}", format!("{}{}", web_fonts_url, api_key));
-    let resp = reqwest::get(format!("{}{}", web_fonts_url, api_key))
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let resp = reqwest::get(format!("{}{}", web_fonts_url, api_key)).await?;
+    let body = resp.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&body)?;
+    let obj = json.as_object().unwrap();
+    for (key, value) in obj.iter() {
+        println!("{}\t{}", key, value);
+    }
     Ok(())
-
 }
